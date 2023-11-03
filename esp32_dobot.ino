02d9c4acb8c423c407eb6a15bd8e40d57153f816
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-11-03 10:08:25 +0100

    Assignment
    Should work...

--- esp32_dobot.ino
+++ esp32_dobot.ino
@@ -1,25 +1,53 @@
+/*
+  Due to a slight oversight, the emergency switch 'feature' had to be removed.
+  Otherwise it wouldn't fit..
+
+  These parts have been commented out 
+*/
+
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 
+// Sensors
 const int trackingPin = 12;
-const int colorPin = 34;
-const int redPin = 27;
-const int greenPin = 25;
-const int orangePin = 26;
+
+// Blocks (color sensor)
+const int redBlock = 25;
+const int greenBlock = 26;
+const int blueBlock = 27;
+
+// Trafficlight
+const int redLight = 15;
+const int yellowLight = 2;
+const int greenLight = 4;
+
+// Misc
+const int dobotMoving = 33;
+// BTN - const int stopButton = 0;
 
 void setup() {
+  // Sensors
   pinMode(trackingPin, INPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(orangePin, OUTPUT);
+  pinMode(dobotMoving, INPUT);
+
+  // Blocks (color sensor)
+  pinMode(redBlock, OUTPUT);
+  pinMode(greenBlock, OUTPUT);
+  pinMode(blueBlock, OUTPUT);
+
+  // Traffic Light
+  pinMode(redLight, OUTPUT);
+  pinMode(yellowLight, OUTPUT);
+  pinMode(greenLight, OUTPUT);
+
   Serial.begin(9600);
 
   if (tcs.begin()) {
-    Serial.println("Found sensor");
+    Serial.println("TCS34725 - Found sensor");
   } else {
-    Serial.println("No TCS34725 found ... check your connections");
+    Serial.println("TCS34725 - No sensor found >> Check your connections");
     while (1)
       ;
   }
@@ -27,59 +55,78 @@ void setup() {
 
 void loop() {
 
-  int sensorValue = analogRead(trackingPin);
-  Serial.print("Sensorwaarde: ");
-  Serial.println(sensorValue);
+  uint16_t r, g, b, c;
+  int sensorValue = digitalRead(trackingPin);
+  int isMoving = digitalRead(dobotMoving);
+  // BTN - int isStopping = digitalRead(stopButton);
+
+  // ???
+  // bool isMoving = digitalRead(dobotMoving) == 1;
+  // bool isStopping = digitalRead(stopButton) == 1;
+
+
+  // Handle lights
+
+  /*
+
+    
+
+    if (isStopping) {
+      Serial.println("isStopping");
+      setLight(HIGH, LOW, LOW);
+    } else 
+  */
+  if (isMoving) {
+    Serial.println("isMoving");
+    setLight(LOW, HIGH, LOW);
+  } else {
+    Serial.println("isIdle");
+    setLight(LOW, LOW, HIGH);
+  }
 
   if (sensorValue != 0) {
-    uint16_t r, g, b, c, colorTemp, lux;
+
+    // A very scuffed solution to make sure the correct color gets detected
+    delay(1000);
+
     tcs.getRawData(&r, &g, &b, &c);
 
     if (r > g && r > b) {
       Serial.println("RED");
-      digitalWrite(greenPin, LOW);
-      digitalWrite(orangePin, LOW);
-      digitalWrite(redPin, HIGH);
+      digitalWrite(greenBlock, LOW);
+      digitalWrite(blueBlock, LOW);
+      digitalWrite(redBlock, HIGH);
+
     } else if (g > r && g > b) {
       Serial.println("GREEN");
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(orangePin, LOW);
-      digitalWrite(redPin, LOW);
+      digitalWrite(greenBlock, HIGH);
+      digitalWrite(blueBlock, LOW);
+      digitalWrite(redBlock, LOW);
+
     } else if (b > r && b > g) {
       Serial.println("BLUE");
-      digitalWrite(greenPin, LOW);
-      digitalWrite(orangePin, HIGH);
-      digitalWrite(redPin, LOW);
+      digitalWrite(greenBlock, LOW);
+      digitalWrite(blueBlock, HIGH);
+      digitalWrite(redBlock, LOW);
+
     } else {
       Serial.println("WTF is dit");
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(orangePin, HIGH);
-      digitalWrite(redPin, HIGH);
+      digitalWrite(greenBlock, HIGH);
+      digitalWrite(blueBlock, HIGH);
+      digitalWrite(redBlock, HIGH);
     }
   } else {
-    Serial.println("Nothing to see here");
-    digitalWrite(greenPin, LOW);
-    digitalWrite(orangePin, LOW);
-    digitalWrite(redPin, HIGH);
+    Serial.println("Not seeing shit");
+    delay(1000);
+
+    digitalWrite(greenBlock, LOW);
+    digitalWrite(blueBlock, LOW);
+    digitalWrite(redBlock, LOW);
   }
+}
 
-  // if (sensorValue > 500) {
-  //   digitalWrite(greenPin, LOW);
-  //   digitalWrite(orangePin, LOW);
-  //   digitalWrite(redPin, HIGH);
-  //   delay(2000);
-  //   digitalWrite(greenPin, LOW);
-  //   digitalWrite(orangePin, HIGH);
-  //   digitalWrite(redPin, LOW);
-  //   delay(1000);
-  //   digitalWrite(greenPin, HIGH);
-  //   digitalWrite(orangePin, LOW);
-  //   digitalWrite(redPin, LOW);
-  //   delay(2000);
-  // } else {
-  //   digitalWrite(greenPin, LOW);
-  //   digitalWrite(redPin, HIGH);
-  // }
-
-  // delay(500);
+void setLight(int red, int yellow, int green) {
+  digitalWrite(redLight, red);
+  digitalWrite(yellowLight, yellow);
+  digitalWrite(greenLight, green);
 }
